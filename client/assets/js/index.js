3f11bad4d443d966361bdc1331ef894b3182bb5a
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2021-10-02 16:54:59 -0500

    Update index.js

---
+++ client/assets/js/index.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+	"https://fmpozdzuibkvfoulqiys.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzE0NTMwMSwiZXhwIjoxOTQ4NzIxMzAxfQ.3hMgFNRbyj9w_5eDpHfWYz879kzZBpvgp5GfpfWJNoI"
+);
